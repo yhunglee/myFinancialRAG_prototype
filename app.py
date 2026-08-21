cee529d7f63c763b7e4af3aab9fa963205d27bfd
Howard Lee <contact-me@howardlee.cloud>
2026-08-21 15:18:17 +0800

    add chainlit's app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+"""
+Chainlit 的主要檔案
+"""
+import chainlit as cl
+
+
+@cl.on_message
+async def main(message: cl.Message):
+  # Your custom logic goes here...
+
+  # Send a response back to the user
+  await cl.Message(
+      content=f"Received: {message.content}",
+  ).send()
