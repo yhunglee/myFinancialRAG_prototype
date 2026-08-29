de377e77fb97484cf34a77b72b1757b15016cdb8
Howard Lee <contact-me@howardlee.cloud>
2026-08-29 22:43:19 +0800

    feat(agent_state.py): modify fields to be common used.

--- agent_state.py
+++ agent_state.py
@@ -2,15 +2,25 @@ from __future__ import annotations
 from typing import TypedDict
 
 class FinancialResearchState(TypedDict):
-
+  # 使用者原始問題
   question: str
-  research_type: str
+
+  # intent_router 輸出
+  intent: str
   companies: list[str]
-  period: str
+  periods: list[str]
+  router_confidence: float
+
+  # research_planner 輸出
   research_plan: list
+  
+  # rag_executer 使用
   current_task: int
   evidence: list
-  sufficent: bool
+
+  # Evidence_checker 使用
+  sufficient: bool
   missing_information: list[str]
+
+  # report_writer 輸出
   final_answer: str
-  
