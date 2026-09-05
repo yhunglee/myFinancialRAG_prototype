6faf54ba32ae36989e5f7d2579190d33652381b2
Howard Lee <contact-me@howardlee.cloud>
2026-09-05 23:05:13 +0800

    feat(agent_graph.py): add more nodes and edges about research_planner and rag_executor().

--- agent_graph.py
+++ agent_graph.py
@@ -3,7 +3,11 @@ from __future__ import annotations
 from langgraph.graph import StateGraph, START, END
 
 from agent_state import FinancialResearchState
-from agent_node import intent_router
+from agent_node import (
+  intent_router,
+  research_planner,
+  rag_executor,
+)
 
 def build_agent_graph():
   graph = StateGraph(
@@ -11,9 +15,13 @@ def build_agent_graph():
   )
 
   graph.add_node("intent_router", intent_router)
+  graph.add_node("research_planner", research_planner)
+  graph.add_node("rag_executor", rag_executor)
 
   # 定義執行流程
   graph.add_edge(START, "intent_router")
-  graph.add_edge("intent_router", END)
+  graph.add_edge("intent_router", "research_planner")
+  graph.add_edge("research_planner", "rag_executor")
+  graph.add_edge("rag_executor", END)
 
   return graph.compile()
